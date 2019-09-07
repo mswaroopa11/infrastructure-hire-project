a3150024116a6cae38431f6ba2df055eec802b0c
iamsam <mswaroopa@gmail.com>
2019-09-07 18:59:11 -0400

    push the docker image to the ECR repo

--- deploy-cloudformation-stack.sh
+++ deploy-cloudformation-stack.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-echo "Run this deployment script everytime you would like to deploy the dockerfile's image to ECR and then use it with the deployed cloudformation stack"
+echo "Run this deployment script everytime you would like to update the dockerfile's image"
 
 # docker files, requirements.txt, app need to be in the same location
 cp ./app/* .
 
-# # create docker image from dockerfile, requirements.txt, app
+# # create docker image (flaskdock) from dockerfile, requirements.txt, app
 docker build -t flaskdock .
 
 # # copies aren't necessary
 rm -rf app.py
 rm -rf wsgi.py
 
-# upload the docker image to ECS/ECR with either cloudformation template?? or with bash
+# upload the docker image to ECR with bash
+## https://docs.aws.amazon.com/AmazonECR/latest/userguide/docker-basics.html
+## follow this documentation to push to ECR. the bash commands dont lead easily to automation. Will take a bit more time than alloted.
+## I manually followed this process to push to ECR. 
 
 
-# docker image in ECR should have some sort of uuid or arn & account id. 
-## this should output from the previous step. 
-## use this id(s) and input into cloudformation so that cloudformation knows which docker image to use
+# sudo apt-get install jq
+
+# aws ecr create-repository --repository-name infrastructure-docker --region us-east-1 > repository.json
+# repositoryUri=$(jq '.repository.repositoryUri' repository.json)
+
+# docker tag flaskdock ${repositoryUri}
+# aws ecr get-login --no-include-email --region us-east-1
+
 
 
 # run the cloudformation deploy
 #aws cloudformation deploy --template-file infrastructure/infrastructure.yml --stack-name ops-hire-project --tags Name=ops-hire-project Environment=dev
+
 
 
